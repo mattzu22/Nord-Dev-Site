cc2e41e8863ab952454b6b1bb522201aa212b22e
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2023-11-17 22:51:55 -0300

    lingua alterada de en para pt-br

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Nord Dev</title>
+      
       <link rel="icon" href="/favicon/favicon.ico"/>
     </>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({children}: {
 children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <Head />
       <body>{children}</body>
     </html>
